fc2a46525166bbc88b25e25f6fc778e167187e33
sia <shamil.iakubov@niva.no>
2024-02-14 13:32:44 +0100

    Replace paths with globs and add natural sort

--- arguments.py
+++ arguments.py
@@ -1,109 +1,42 @@
+import re
+import glob
 from dataclasses import dataclass
+
+def natural_sort(l):  # noqa: E741
+    convert = lambda text: int(text) if text.isdigit() else text.lower()  # noqa: E731
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]  # noqa: E731
+    return sorted(l, key=alphanum_key)
+
 
 @dataclass
 class ArgumentsSpring2017:
     start_time: str = '2017-02-01'
     end_time: str = '2017-04-01'
     input_grid_file: str = '/cluster/projects/nn9490k/ROHO800/Grid/ROHO800_grid_fix5.nc'
-    roho800orig_files: tuple = (
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0525.nc',
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0526.nc',
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0527.nc',
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0528.nc',
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0529.nc',
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0530.nc',
-        '/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_0531.nc',
+    roho800orig_files: tuple = tuple(
+        natural_sort(
+            glob.glob('/cluster/projects/nn9490k/ROHO800/STORAGE/ROHO800_hindcast_2007_2019_v2bn/roho800_v2bn_his/roho800_his_05*.nc')
+        )
     )
-    roho800_files: tuple = (
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/forward/3_2017_01_15-2019_12_27/his_0003.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/forward/3_2017_01_15-2019_12_27/his_0004.nc',
+    roho800_files: tuple = tuple(
+        natural_sort(
+            glob.glob('/cluster/projects/nn9297k/shmiak/roho800_data/output_data/forward/3_2017_01_15-2019_12_27/his_000*.nc')
+        )
     )
-    roho160_files: tuple = (
-        '/cluster/projects/nn9297k/shmiak/roho160_data/3_2017-01-15_to_2019-07-16_with_AKx_no_lakes/roho160_his_002.nc',
-        '/cluster/projects/nn9297k/shmiak/roho160_data/3_2017-01-15_to_2019-07-16_with_AKx_no_lakes/roho160_his_003.nc',
-        '/cluster/projects/nn9297k/shmiak/roho160_data/3_2017-01-15_to_2019-07-16_with_AKx_no_lakes/roho160_his_004.nc',
+    roho160_files: tuple = tuple(
+        natural_sort(
+            glob.glob('/cluster/projects/nn9297k/shmiak/roho160_data/3_2017-01-15_to_2019-07-16_with_AKx_no_lakes/roho160_his_00*.nc')
+        )
     )
-    baseline_files: tuple = (
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/5_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/6_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/7_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/8_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/9_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/10_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/11_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/12_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/13_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/14_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/15_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/16_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/17_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/18_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/19_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/20_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/21_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/22_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/23_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/24_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/25_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/26_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/27_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/28_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/29_iter/roho800_his_0001.nc',
+    baseline_files: tuple = tuple(
+        natural_sort(
+            glob.glob('/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_forward/y2017/*_iter/roho800_his_0001.nc')
+        )
     )
-    nature_files: tuple = (
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/17_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/18_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/19_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/20_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/21_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/22_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/23_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/24_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/25_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/26_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/27_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/28_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/29_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/30_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/31_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/32_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/33_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/34_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/35_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/36_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/37_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/38_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/39_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/40_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/41_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/42_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/43_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/44_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/45_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/46_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/47_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/48_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/49_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/50_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/51_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/52_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/53_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/54_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/55_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/56_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/57_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/58_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/59_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/60_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/61_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/62_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/63_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/64_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/65_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/66_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/67_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/68_iter/roho800_his_0001.nc',
-        '/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/69_iter/roho800_his_0001.nc',
+    nature_files: tuple = tuple(
+        natural_sort(
+            glob.glob('/cluster/projects/nn9297k/shmiak/roho800_data/output_data/orig_with_phys_from_roho160/2017_3_months/*_iter/roho800_his_0001.nc')
+        )
     )
     stations: str = '/cluster/projects/nn9297k/shmiak/roho160_data/1_2017-01-15_to_2019-07-16/roho160_sta.nc'
     wc13_obs_file: str = '/cluster/home/shmiak/src/roms-applications/WC13/Data/wc13_obs.nc'
